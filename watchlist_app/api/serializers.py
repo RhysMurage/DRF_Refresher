8513eccaf5ed0908c32d0d5418bd1e3438f0639e
RhysMurage <rhysmwangi96@gmail.com>
2021-11-15 23:04:06 +0300

    Serialization

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -9,3 +9,10 @@ class MovieSerializer(serializers.Serializer):
 
     def create(self,validated_data):
         return Movie.objects.create(**validated_data)
+
+    def update(self,instance,validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance)
+        instance.active = validated_data.get('active', instance.active)
+        instance.ave()
+        # returm instance
